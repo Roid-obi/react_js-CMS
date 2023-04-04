694d8e042b5300bb7bdff991f23ed924b73d2686
Roid-obi <contohdiamazon2@gmail.com>
2023-04-04 08:56:43 +0700

    tabel jadi card

--- front-end/src/Pages/Posts/Index.jsx
+++ front-end/src/Pages/Posts/Index.jsx
@@ -1,44 +1,39 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Button, Container, Table } from "react-bootstrap";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 
 export default function PostIndex() {
-    const [posts, setPosts] = useState([]);
-    useEffect(() => {
-        async function fetchData() {
-            const response = await axios.get('http://localhost:8000/api/posts');
-            setPosts(response.data.posts.data);
-            console.log(response.data.posts.data);
-        }
-        fetchData();
-    }, []);
-    return (
+  const [posts, setPosts] = useState([]);
+  
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get('http://localhost:8000/api/posts');
+      setPosts(response.data.posts.data);
+      console.log(response.data.posts.data);
+    }
+    fetchData();
+  }, []);
+  
+  return (
     <div>
-        <Container className="mt-3">
+      <Container className="mt-3">
         <Button href="/posts/create" variant="dark" className="mb-3">Create Post</Button>
-            <Table className="shadow-sm" striped bordered size="md">
-            <thead>
-                <tr>
-                <th width='5%'>No.</th>
-                <th>Title</th>
-                <th>Content</th>
-                <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                {posts.map((post, index) => (
-                <tr key={post.id}>
-                    <td>{index + 1}</td>
-                    <td>{post.title}</td>
-                    <td>{post.body}</td>
-                    <td>
-                        <a href={`/post/${post.id}`}>Show</a>
-                    </td>
-                </tr>
-                ))}
-            </tbody>
-            </Table>
-        </Container>
+        <Row xs={1} sm={2} md={3} lg={4} className="g-4">
+          {posts.map((post, index) => (
+            <Col key={post.id}>
+              <Card>
+                <Card.Body>
+                  <Card.Title>{post.title}</Card.Title>
+                  <Card.Text>
+                    {post.body}
+                  </Card.Text>
+                  <Button href={`/post/${post.id}`} variant="dark">Show</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
-    );
+  );
 }
